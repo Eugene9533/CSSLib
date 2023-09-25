b188d909f6ce871a8aaf5691cccbc3c7d3918c5c
EugeneS <zhenya9533@gmail.com>
2023-09-25 16:51:03 +0300

    sections added

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-let cards = document.querySelector(".cards");
+let elements = document.querySelector(".cards.elements");
+let sites = document.querySelector(".cards.sites");
 
 (async function () {
-  let data = await fetch("./data.json");
+  let data = await fetch("./dataElements.json");
   let parseData = await data.json();
 
   async function createCards(parseData) {
@@ -15,11 +16,33 @@ let cards = document.querySelector(".cards");
                           </div>
                         </div>
                       </a>`;
-      cards.appendChild(div);
+      elements.appendChild(div);
     }
   }
 
   createCards(parseData);
+})();
+
+(async function () {
+  let data = await fetch("./dataSites.json");
+  let parseData = await data.json();
+
+  async function createSites(parseData) {
+    for (let item of parseData) {
+      let div = document.createElement("div");
+      div.className = item.id;
+      div.innerHTML = `<a href=${item.href}>
+                        <div class="container" style="background-image: url(${item.img})">
+                          <div class="description">
+                            <div class="text">${item.description}</div>
+                          </div>
+                        </div>
+                      </a>`;
+      sites.appendChild(div);
+    }
+  }
+
+  createSites(parseData);
 })();
 
 window.addEventListener("scroll", function () {
