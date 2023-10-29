6e1d22f80e358e780eb8096a19e4a7875e392800
EugeneS <zhenya9533@gmail.com>
2023-10-29 21:52:23 +0300

    Cards

---
+++ Lib/Cards/CardsStyle/06/app.js
@@ -0,0 +1,11 @@
+let cardsSix = document.querySelectorAll(".containerSix .card");
+
+cardsSix.forEach((card) => {
+  card.onmousemove = function (e) {
+    let x = e.pageX - card.offsetLeft;
+    let y = e.pageY - card.offsetTop;
+
+    card.style.setProperty("--x", x + "px");
+    card.style.setProperty("--y", y + "px");
+  };
+});
