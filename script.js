b37ed377c496289619c5dd7577c8c1f5bd4a1a82
EugeneS <zhenya9533@gmail.com>
2023-11-30 03:09:52 +0300

    footer, names elements, createCards

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let elements = document.querySelector(".cards.elements");
-let sites = document.querySelector(".cards.sites");
+let css = document.querySelector(".cards.css");
+let site = document.querySelector(".cards.site");
+let ga = document.querySelector(".cards.ga");
 let getC = JSON.parse(sessionStorage.getItem("visibleCards"));
 let setC = 0;
 
@@ -11,8 +12,24 @@ if (getC > setC) setC = getC;
 
     async function createCards(parseData) {
         for (let item of parseData) {
+            switch (item.type) {
+                case "css":
+                    card(item, css);
+                    break;
+                case "ga":
+                    card(item, ga);
+                    break;
+                case "site":
+                    card(item, site);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        function card(item, type) {
             let div = document.createElement("div");
-            div.className = `card ${item.id}`;
+            div.className = `card ${item.type}`;
             div.innerHTML = `<a href=${item.href}>
                         <div class="container" style="background-image: url(${item.img})">
                           <div class="description">
@@ -20,35 +37,11 @@ if (getC > setC) setC = getC;
                           </div>
                         </div>
                       </a>`;
-            elements.appendChild(div);
+            type.appendChild(div);
         }
     }
 
     createCards(parseData);
-    sessionStorageVisibleCards();
-    showElem();
-})();
-
-(async function () {
-    let data = await fetch("./dataSites.json");
-    let parseData = await data.json();
-
-    async function createSites(parseData) {
-        for (let item of parseData) {
-            let div = document.createElement("div");
-            div.className = `card ${item.id}`;
-            div.innerHTML = `<a href=${item.href}>
-                        <div class="container" style="background-image: url(${item.img})">
-                          <div class="description">
-                            <div class="text">${item.description}</div>
-                          </div>
-                        </div>
-                      </a>`;
-            sites.appendChild(div);
-        }
-    }
-
-    createSites(parseData);
     sessionStorageVisibleCards();
     showElem();
 })();
