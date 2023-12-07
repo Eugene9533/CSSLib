8b1a86d70d5b3b945bd2d811af4c509b348bce4b
EugeneS <zhenya9533@gmail.com>
2023-12-08 01:08:22 +0300

    all files directories are changed

---
+++ collection/3DAnimated/script.js
@@ -0,0 +1,50 @@
+let Y = 50;
+let X = 0;
+let dirX = 0;
+let degS = 360;
+let degE = 0;
+
+let animate = document.querySelector(".city");
+document.querySelector(
+    ".city"
+).style = `--degS:${degS}deg; --degE:${degE}deg; transform: rotateX(-30deg) rotateY(${degS}deg)`;
+
+window.addEventListener("wheel", (e) => {
+    let n = e.deltaY / 100;
+    if (Y > 5 && Y < 100) {
+        Y -= n;
+    }
+
+    document.querySelector(".scale").style.transform = `scale(${Y / 50})`;
+
+    if (Y === 5) Y = 6;
+    if (Y === 100) Y = 99;
+});
+
+window.addEventListener("mousedown", () => {
+    document.onmousemove = function (e) {
+        dirX = X;
+        X = e.clientX / 10;
+
+        if (dirX > X) {
+            degS -= 3;
+        } else {
+            degS += 3;
+        }
+
+        degE = degS - 360;
+
+        document.querySelector(
+            ".city"
+        ).style = `--degS:${degS}deg; --degE:${degE}deg; transform: rotateX(-30deg) rotateY(${degS}deg)`;
+    };
+
+    animate.classList.toggle("animate");
+    animate.classList.toggle("pause");
+});
+
+window.addEventListener("mouseup", () => {
+    document.onmousemove = null;
+    animate.classList.toggle("animate");
+    animate.classList.toggle("pause");
+});
---
+++ src/elements/return_button/script.js
@@ -0,0 +1,14 @@
+function addReturnButton() {
+    fetch("../../src/elements/return_button/index.html")
+        .then((resp) => resp.text())
+        .then((data) => {
+            document.querySelector(".return").innerHTML = data;
+        });
+
+    let link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = "../../src/elements/return_button/style.css";
+    document.getElementsByTagName("head")[0].appendChild(link);
+}
+addReturnButton();
