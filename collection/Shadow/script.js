b1e6573d90220a2d12dac9f1236f9aeb6fc1d82c
EugeneS <zhenya9533@gmail.com>
2024-04-29 12:09:36 +0300

    individual shadows

--- collection/Shadow/script.js
+++ collection/Shadow/script.js
@@ -1,23 +1,27 @@
-let text = document.getElementById("text");
+let text = document.querySelectorAll("#text");
 let light = document.getElementById("light");
+let a = document.querySelectorAll("#text");
 
 document.addEventListener("mousemove", (event) => {
     let mouseX = event.clientX;
     let mouseY = event.clientY;
-    let distanceX = mouseX - text.offsetLeft - text.offsetWidth / 2;
-    let distanceY = mouseY - text.offsetTop - text.offsetHeight / 2;
 
     light.style.left = mouseX + "px";
     light.style.top = mouseY + "px";
 
-    let newShadow = "";
-    for (let i = 0; i < 200; i++) {
-        let shadowX = -distanceX * (i / 200);
-        let shadowY = -distanceY * (i / 200);
-        let opacity = 1 - i / 100;
-        newShadow += `${
-            newShadow ? "," : ""
-        }${shadowX}px ${shadowY}px 0 rgba(33, 33, 33,${opacity})`;
-    }
-    text.style.textShadow = newShadow;
+    text.forEach((elem) => {
+        let newShadow = "";
+        let distanceX = mouseX - elem.offsetLeft - elem.offsetWidth / 2;
+        let distanceY = mouseY - elem.offsetTop - elem.offsetHeight / 2;
+
+        for (let i = 0; i < 200; i++) {
+            let shadowX = -distanceX * (i / 200);
+            let shadowY = -distanceY * (i / 200);
+            let opacity = 1 - i / 100;
+            newShadow += `${
+                newShadow ? "," : ""
+            }${shadowX}px ${shadowY}px 0 rgba(33, 33, 33,${opacity})`;
+        }
+        elem.style.textShadow = newShadow;
+    });
 });
