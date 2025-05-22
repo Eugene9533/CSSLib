60fa27857541fb4180f7e42ee96c91e4c1ded5c0
Eugene9533 <zhenya9533@gmail.com>
2025-05-22 17:57:25 +0300

    fix ico, styles

--- src/elements/return_button/script.js
+++ src/elements/return_button/script.js
@@ -1,5 +1,5 @@
 function addReturnButton() {
-    fetch("../../src/elements/return_button/index.html")
+    fetch("../../../src/elements/return_button/index.html")
         .then((resp) => resp.text())
         .then((data) => {
             document.querySelector(".return").innerHTML = data;
@@ -8,7 +8,7 @@ function addReturnButton() {
     let link = document.createElement("link");
     link.rel = "stylesheet";
     link.type = "text/css";
-    link.href = "../../src/elements/return_button/style.css";
+    link.href = "../../../src/elements/return_button/style.css";
     document.getElementsByTagName("head")[0].appendChild(link);
 }
 addReturnButton();
